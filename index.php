e77896f3f1416070e843a8038ea038ea1fe114b6
chandupant07 <chandupant1111@gmail.com>
2025-04-24 21:47:25 +0530

    complate

--- index.php
+++ index.php
@@ -11,16 +11,42 @@
 </head>
 
 <body>
-  <form action="" method="POST" enctype="">
+  <form action="" method="POST" enctype="multipart/form-data">
     <div class="container">
       <div class="mb-3">
         <label for="formFile" class="form-label">Default file input example</label>
-        <input class="form-control" type="file" id="formFile">
+        <input class="form-control" type="file" id="formFile" name="file">
       </div>
 
       <input type="submit" value="submit" name="submit" class="btn btn-success" />
     </div>
   </form>
+  <?php
+  if (isset($_POST['submit'])) {
+    if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+      $fileName = $_FILES['file']['name'];
+      $fTmp = $_FILES['file']['tmp_name'];
+      $type = $_FILES['file']['type'];
+      $fileType = ['image/jpeg', 'image/png', 'image/jpg'];
+
+      if (in_array($type, $fileType)) {
+        if (file_exists('upload/' . $fileName)) {
+          $str = substr(str_shuffle('helloWelcome'), 10, 10);
+          $fileName = $str . "_" . time() . "_" . $fileName;
+        }
+        if (move_uploaded_file($fTmp, "upload/" . $fileName)) {
+          echo "<p>success! profile photo update successfully</p>";
+        } else {
+          echo "<p>Sorry ! unable to upload file</p>";
+        }
+      } else {
+        echo "select a valid file formate";
+      }
+    }
+  } else {
+    echo "Please upload a profile pic";
+  }
+  ?>
 </body>
 
 </html>
